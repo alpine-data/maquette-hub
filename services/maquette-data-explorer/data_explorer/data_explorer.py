924420af46688c49cf53f3bc3127f7f31ab8abda
AiPunk <johannes.forster@gmail.com>
2021-01-26 19:55:57 +0100

    switch to maquette instead of maquette_sdk

--- services/maquette-data-explorer/data_explorer/data_explorer.py
+++ services/maquette-data-explorer/data_explorer/data_explorer.py
@@ -1,9 +1,8 @@
-from maquette_sdk import Dataset
+from maquette import Dataset
 from data_explorer.data_analyzer import generate_df_statistics
 from data_explorer.models.response import ResponseBody
 
 import pandas as pd
-
 
 def __load_df(dataset_name: str, version: str="") -> pd.DataFrame:
     return Dataset(dataset_name).version(version).get()
