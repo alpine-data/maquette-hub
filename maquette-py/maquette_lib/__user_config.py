de1211de296242ecfafd8c47b556985c960d3ed9
AiPunk <johannes.forster@gmail.com>
2021-02-10 15:50:45 +0100

    merged json config to yaml

--- maquette-py/maquette_lib/__user_config.py
+++ maquette-py/maquette_lib/__user_config.py
@@ -2,24 +2,24 @@ import os
 import yaml
 import pathlib
 
-class UserConfiguration:
+class EnvironmentConfiguration:
 
 #TODO authentication only implemented for stupid(s), implement the other 2
 #TODO auto dump env variables similar to unix_env.py (then this can be replaced)
-    __mq_config: str = None
-    __mq_yaml_list = []
+    mq_config: str = None
+    mq_yaml_list = []
 
     def __init__(self, mq_home: str = None):
         if mq_home:
-            self.__mq_config = os.path.join(mq_home,"config.yaml")
+            self.mq_config = os.path.join(mq_home, "config.yaml")
         else:
-            self.__mq_config = os.path.join(pathlib.Path.home(), ".mq", "config.yaml")
-        if os.path.exists(self.__mq_config):
-            with open(self.__mq_config) as file:
-                self.__mq_yaml_list = yaml.load(file, Loader=yaml.FullLoader)
+            self.mq_config = os.path.join(pathlib.Path.home(), ".mq", "config.yaml")
+        if os.path.exists(self.mq_config):
+            with open(self.mq_config) as file:
+                self.mq_yaml_list = yaml.load(file, Loader=yaml.FullLoader)
 
     def get_url(self) -> str:
-        url = self.__mq_yaml_list.get('url', 'http://localhost:9042')
+        url = self.mq_yaml_list.get('url', 'http://localhost:9042')
 
         if not (url.endswith('/api/') or url.endswith('/api')):
             url = f"{url}/api/"
@@ -30,42 +30,53 @@ class UserConfiguration:
         return os.environ.get('MQ_BASE_URL', url)
 
     def get_user(self) -> str:
-        if 'authentication' in self.__mq_yaml_list:
-            user =  self.__mq_yaml_list['authentication'].get('username', 'alice')
+        if 'authentication' in self.mq_yaml_list:
+            user =  self.mq_yaml_list['authentication'].get('username', 'alice')
         else:
             user = 'alice'
         return os.environ.get('MQ_USERNAME', user)
 
     def get_roles(self):
-        if 'authentication' in self.__mq_yaml_list:
-            roles = self.__mq_yaml_list['authentication'].get('roles', 'a-team, b-team')
+        if 'authentication' in self.mq_yaml_list:
+            roles = self.mq_yaml_list['authentication'].get('roles', 'a-team, b-team')
         else:
             roles = 'a-team, b-team'
         return os.environ.get('MQ_ROLES', 'a-team, b-team').split(", ")
 
     def get_project(self) -> str:
-        if 'project' in self.__mq_yaml_list:
-            project_id = self.__mq_yaml_list['project'].get('id', None)
+        if 'project' in self.mq_yaml_list:
+            project_id = self.mq_yaml_list['project'].get('id', None)
         else:
             project_id = None
         return os.environ.get('MQ_PROJECT_ID', project_id)
 
     def get_project_name(self) -> str:
-        if 'project' in self.__mq_yaml_list:
-            project_name = self.__mq_yaml_list['project'].get('name', None)
+        if 'project' in self.mq_yaml_list:
+            project_name = self.mq_yaml_list['project'].get('name', None)
         else:
             project_name = None
         return os.environ.get('MQ_PROJECT_NAME', project_name)
 
     def activate_project(self, project_name, project_id):
-        if self.__mq_config:
-            self.__mq_yaml_list["project"]["id"] = project_id
-            self.__mq_yaml_list["project"]["name"] = project_name
-            with open(self.__mq_config, 'w') as file:
-                yaml.dump(self.__mq_yaml_list, file)
+        if self.mq_config:
+            self.mq_yaml_list["project"]["id"] = project_id
+            self.mq_yaml_list["project"]["name"] = project_name
+            with open(self.mq_config, 'w') as file:
+                yaml.dump(self.mq_yaml_list, file)
 
 
-    def deactivate_project(self):
-        if self.__mq_config:
-            with open(self.__mq_config, 'w') as file:
-                yaml.dump(self.__mq_yaml_list, file)
+    def add_process_env(self, key, value):
+        if 'environment' in self.mq_yaml_list:
+            self.mq_yaml_list['environment'][key] = value
+        else:
+            self.mq_yaml_list['environment'] = {}
+            self.mq_yaml_list['environment'][key] = value
+        if self.mq_config:
+            with open(self.mq_config, 'w') as file:
+                yaml.dump(self.mq_yaml_list, file)
+
+    def remove_process_envs(self):
+        self.mq_yaml_list.pop('environment', None)
+        if self.mq_config:
+            with open(self.mq_config, 'w') as file:
+                yaml.dump(self.mq_yaml_list, file)
