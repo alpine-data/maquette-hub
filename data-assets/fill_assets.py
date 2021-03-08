29c8a369ecbf5e7d6def0028752f96c9d5540651
AiPunk <johannes.forster@gmail.com>
2021-03-08 16:41:53 +0100

    added new prediction set for ai in control

--- data-assets/fill_assets.py
+++ data-assets/fill_assets.py
@@ -1,7 +1,6 @@
 from maquette import *
 import pandas as pd
 
-
 def init_weather():
     # Upload Weather Dataset to Sample Project
     Dataset('city-temperature', title='City Temperatures', summary="Historical weather data of major cities of Europe").create()
@@ -22,6 +21,35 @@ def init_fraud():
     #print("Checking success:")
     #print(dataset('fraud-analysis').get())
 
+def init_house_prices_training():
+    Dataset('house-prices-training', title='House Prices', summary="The Ames Housing dataset was compiled by Dean De Cock for use in data science education. It's an incredible alternative for data scientists looking for a modernized and expanded version of the often cited Boston Housing dataset. This is the training set", classification="public", personal_information="none").create()
+    train_csv = 'sources/house-prices/train.csv'
+    train_df = pd.read_csv(train_csv, na_values="")
+    filter_columns = ['Alley', 'PoolQC', 'MiscFeature', 'FireplaceQu', 'Fence']
+    train_df.drop(filter_columns, inplace=True, axis=1)
+    train_df.dropna(axis=1, inplace=True)
+    train_df.columns = train_df.columns.str.replace("1", "Fir")
+    train_df.columns = train_df.columns.str.replace("2", "Seco")
+    train_df.columns = train_df.columns.str.replace("3", "Thi")
+    dataset('house-prices-training').put(train_df, "house prices train init")
+
+    print("Uploaded House Prices Trainings Dataset")
+
+def init_house_prices_test():
+    Dataset('house-prices-test', title='House Prices', summary="The Ames Housing dataset was compiled by Dean De Cock for use in data science education. It's an incredible alternative for data scientists looking for a modernized and expanded version of the often cited Boston Housing dataset. This is the test set", classification="public", personal_information="none").create()
+    test_csv = 'sources/house-prices/test.csv'
+    test_df = pd.read_csv(test_csv, na_values="")
+    filter_columns = ['Alley', 'PoolQC', 'MiscFeature', 'FireplaceQu', 'Fence']
+    test_df.drop(filter_columns, inplace=True, axis=1)
+    test_df.dropna(axis=1, inplace=True)
+    test_df.columns = test_df.columns.str.replace("1", "Fir")
+    test_df.columns = test_df.columns.str.replace("2", "Seco")
+    test_df.columns = test_df.columns.str.replace("3", "Thi")
+    dataset('house-prices-test').put(test_df, "house prices test init")
+
+    print("Uploaded House Prices Test Dataset")
 
 init_weather()
 init_fraud()
+init_house_prices_training()
+init_house_prices_test()
