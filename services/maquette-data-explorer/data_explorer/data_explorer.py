b44db436b1dc81cfe932adcd1bdcd6085fac5973
AiPunk <johannes.forster@gmail.com>
2021-05-25 18:44:08 +0200

    added pandas profiling as alternative for advanced data exploration

--- services/maquette-data-explorer/data_explorer/data_explorer.py
+++ services/maquette-data-explorer/data_explorer/data_explorer.py
@@ -1,14 +1,16 @@
 from maquette import Dataset, Source
 from data_explorer.data_analyzer import generate_df_statistics
 from data_explorer.models.response import ResponseBody
+from data_explorer.models.request import FormatEnum
 
 import pandas as pd
+from pandas_profiling import ProfileReport
 
-def __load_df(dataset_name: str = None, source: str=None, version: str=None) -> pd.DataFrame:
-    if dataset_name:
-        return Dataset(dataset_name).get(version=version)
-    elif source:
-        return Source(source).get()
+def __load_df(data_asset_name: str, version: str=None) -> pd.DataFrame:
+    if version:
+        return Dataset(data_asset_name).get(version=version)
+    else:
+        return Source(data_asset_name).get()
 
 
 def get_ds_statistics(dataset_name: str, version: str=None, with_images=False) -> ResponseBody:
@@ -16,4 +18,14 @@ def get_ds_statistics(dataset_name: str, version: str=None, with_images=False) -
 
 def get_so_statistics(source_name: str, with_images=False) -> ResponseBody:
     return ResponseBody(columns=generate_df_statistics(__load_df(source_name), with_images), source=source_name)
+
+def get_profile(data_asset_name: str, format: FormatEnum, version: str=None):
+    df = __load_df(data_asset_name, version)
+    profile = ProfileReport(df, title=data_asset_name+" Report")
+    if format == FormatEnum.json:
+        return profile.to_json()
+    else:
+        return profile.to_html()
+
+
 
--- services/maquette-data-explorer/service.py
+++ services/maquette-data-explorer/service.py
@@ -1,14 +1,27 @@
+import uvicorn
+
 from fastapi import FastAPI
-from data_explorer.models.request import RequestBody
+from data_explorer.models.request import RequestBody, FormatEnum
 from data_explorer.models.response import ResponseBody
-from data_explorer.data_explorer import get_ds_statistics
+from data_explorer.data_explorer import get_ds_statistics, get_so_statistics, get_profile
 
 app = FastAPI()
 
 
 @app.post("/api/statistics", response_model=ResponseBody)
 async def statistics(request: RequestBody, plots: bool = False):
+        if request.dataset:
+            return get_ds_statistics(dataset_name=request.dataset, version=request.version, with_images=plots)
+        elif request.source:
+            return get_so_statistics(source_name=request.source, with_images=plots)
+
+
+@app.post("/api/profile")
+async def profile(request: RequestBody):
     if request.dataset:
-        return get_ds_statistics(dataset_name=request.dataset, version=request.version, with_images=plots)
+        return get_profile(data_asset_name=request.dataset, version=request.version, format=request.format)
     elif request.source:
-        return get_ds_statistics(source_name=request.source, with_images=plots)
+        return get_profile(data_asset_name=request.source, format=request.format)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
