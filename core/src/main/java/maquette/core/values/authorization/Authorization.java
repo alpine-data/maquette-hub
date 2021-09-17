cb4b3c20d20b2f2ad4f197022d92e0c16d98f8cd
Michael Wellner <michaelwellner@kpmg.com>
2021-09-17 15:24:14 +0200

    Fixed JSON sub-type configuration.

--- core/src/main/java/maquette/core/values/authorization/Authorization.java
+++ core/src/main/java/maquette/core/values/authorization/Authorization.java
@@ -1,7 +1,18 @@
 package maquette.core.values.authorization;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import maquette.core.values.user.User;
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    property = "type")
+@JsonSubTypes(
+    {
+        @JsonSubTypes.Type(value = RoleAuthorization.class, name = "role"),
+        @JsonSubTypes.Type(value = UserAuthorization.class, name = "user"),
+        @JsonSubTypes.Type(value = WildcardAuthorization.class, name = "wildcard")
+    })
 public interface Authorization {
 
     boolean authorizes(User user);
