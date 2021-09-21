a8c4937b834a35719e10b94c4c518eaf586edf32
Michael Wellner <michaelwellner@kpmg.com>
2021-09-21 18:50:28 +0200

    fixed de-serialization errors

--- data-shop-impl/src/main/java/maquette/datashop/providers/datasets/model/CommittedRevision.java
+++ data-shop-impl/src/main/java/maquette/datashop/providers/datasets/model/CommittedRevision.java
@@ -1,5 +1,7 @@
 package maquette.datashop.providers.datasets.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Value;
@@ -15,27 +17,53 @@ import java.util.Optional;
 @AllArgsConstructor(staticName = "apply")
 public class CommittedRevision implements Revision {
 
+   private static final String ID = "id";
+   private static final String CREATED = "created";
+   private static final String MODIFIED = "modified";
+   private static final String COMMITTED = "committed";
+   private static final String RECORDS = "records";
+   private static final String SCHEMA = "schema";
+   private static final String VERSION = "version";
+   private static final String MESSAGE = "message";
+   private static final String STATISTICS = "statistics";
+
+   @JsonProperty(ID)
    UID id;
 
+   @JsonProperty(CREATED)
    ActionMetadata created;
 
+   @JsonProperty(MODIFIED)
    ActionMetadata modified;
 
+   @JsonProperty(COMMITTED)
    ActionMetadata committed;
 
+   @JsonProperty(RECORDS)
    long records;
 
+   @JsonProperty(SCHEMA)
    Schema schema;
 
+   @JsonProperty(VERSION)
    DatasetVersion version;
 
+   @JsonProperty(MESSAGE)
    String message;
 
+   @JsonProperty(STATISTICS)
    JsonNode statistics;
 
+   @JsonCreator
    public static CommittedRevision apply(
-      UID id, ActionMetadata created, ActionMetadata modified, ActionMetadata committed,
-      long records, Schema schema, DatasetVersion version, String message) {
+      @JsonProperty(ID) UID id,
+      @JsonProperty(CREATED) ActionMetadata created,
+      @JsonProperty(MODIFIED) ActionMetadata modified,
+      @JsonProperty(COMMITTED) ActionMetadata committed,
+      @JsonProperty(RECORDS) long records,
+      @JsonProperty(SCHEMA) Schema schema,
+      @JsonProperty(VERSION) DatasetVersion version,
+      @JsonProperty(MESSAGE) String message) {
 
       return apply(id, created, modified, committed, records, schema, version, message, null);
    }
