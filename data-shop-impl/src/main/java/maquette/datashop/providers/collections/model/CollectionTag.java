3433409c06b5da183bd2454e3ffa756267ac92f6
janwithb <janwithb@gmail.com>
2022-08-11 13:23:45 +0200

    bug fix: no JSONCreator was available before

--- data-shop-impl/src/main/java/maquette/datashop/providers/collections/model/CollectionTag.java
+++ data-shop-impl/src/main/java/maquette/datashop/providers/collections/model/CollectionTag.java
@@ -1,12 +1,22 @@
 package maquette.datashop.providers.collections.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import maquette.core.values.ActionMetadata;
 
 @Value
-@AllArgsConstructor(staticName = "apply")
+@AllArgsConstructor()
 public class CollectionTag {
+
+   private static final String CREATED = "created";
+
+   private static final String NAME = "name";
+
+   private static final String MESSAGE = "message";
+
+   private static final String CONTENT = "content";
 
    ActionMetadata created;
 
@@ -16,4 +26,12 @@ public class CollectionTag {
 
    FileEntry.Directory content;
 
+   @JsonCreator
+   public static CollectionTag apply(
+           @JsonProperty(CREATED) ActionMetadata created,
+           @JsonProperty(NAME) String name,
+           @JsonProperty(MESSAGE) String message,
+           @JsonProperty(CONTENT) FileEntry.Directory content) {
+      return new CollectionTag(created, name, message, content);
+   }
 }
