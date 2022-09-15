f500630c8f70370a1e255f57d56876d3817107cb
Michael Wellner <michaelwellner@kpmg.com>
2022-09-15 11:14:35 +0200

    Added dummy provider settings as default settings to avoid jackson issues.

---
+++ data-shop-impl/src/main/java/maquette/datashop/providers/NoDataAssetSettings.java
@@ -0,0 +1,11 @@
+package maquette.datashop.providers;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Default settings class.
+ */
+@AllArgsConstructor(staticName = "apply")
+public final class NoDataAssetSettings implements DataAssetSettings {
+
+}
